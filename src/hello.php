b92c247f8d72f5dfd6d06bce961133cecb3049b2
GreyGonz <gerardrey@iesebre.com>
2017-10-11 21:38:22 +0200

    Sync

--- src/hello.php
+++ src/hello.php
@@ -1,3 +1,12 @@
-class Hello() {
+<?php
 
-}
+Class Hello {
+
+
+
+    public function hello() {
+        $faker = Faker\Factory::create();
+        echo "Hello " . $faker->name;
+
+    }
+};
---
+++ test/text_hello.php
@@ -0,0 +1,9 @@
+<?php
+
+require "../vendor/autoload.php";
+
+require "../src/hello.php";
+
+$hello = new Hello;
+
+$hello->hello();
